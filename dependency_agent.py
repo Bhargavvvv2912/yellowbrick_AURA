e9a3375ef24ab7407338a342978744004479513e
Bhargav <bhargavksrinidhi@gmail.com>
2026-02-07 18:24:08 -0500

    Files added

--- dependency_agent.py
+++ dependency_agent.py
@@ -31,7 +31,7 @@ AGENT_CONFIG = {
     "PRIMARY_REQUIREMENTS_FILE": "primary_requirements.txt",
     "VALIDATION_CONFIG": {
         "type": "script",
-        "smoke_test_script": "validation_transformer.py",
+        "smoke_test_script": "validation_yellowbrick.py",
         "project_dir": "." 
     },
     "MAX_RUN_PASSES": 3,
